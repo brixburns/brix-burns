a87939cffd5836015a9d7ea0ca188171f747330d
brixburns <420losrs@gmail.com>
2026-05-10 16:22:41 +0200

    update favicon v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,12 @@ export const metadata: Metadata = {
         sizes: "any",
       },
       {
-        url: "/favicon-32x32.png",
+        url: "/favicon-v2-32x32.png",
         type: "image/png",
         sizes: "32x32",
       },
       {
-        url: "/favicon-16x16.png",
+        url: "/favicon-v2-16x16.png",
         type: "image/png",
         sizes: "16x16",
       },
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
 
     apple: [
       {
-        url: "/apple-touch-icon.png",
+        url: "/apple-touch-v2-icon.png",
         sizes: "180x180",
         type: "image/png",
       },
@@ -43,29 +43,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-  <head>
-    <link rel="icon" href="/favicon.ico" sizes="any" />
+ <head>
+  <meta name="theme-color" content="#000000" />
 
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/favicon-32x32.png"
-    />
+  <link rel="icon" href="/favicon.ico" sizes="any" />
 
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/favicon-16x16.png"
-    />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/favicon-v2-32x32.png"
+  />
 
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/apple-touch-icon.png"
-    />
-  </head>
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="/favicon-v2-16x16.png"
+  />
+
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="/apple-touch-v2-icon.png"
+  />
+</head>
 
   <body>{children}</body>
 </html>
